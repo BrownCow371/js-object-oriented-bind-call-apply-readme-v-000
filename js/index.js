3c342a4e6393913aed7b757165f99a7a812ef4dd
Jamie Brown <jmbrowncow@gmail.com>
2018-09-20 13:32:45 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -10,3 +10,28 @@ let newGreet = greet.bind(sally);
 newGreet('Bob');
 
 greet('Bob');
+
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+ 
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+ 
+    matchInterests(event) {
+        return event.keywords.some(function(word) {
+            return this.interests.includes(word);
+        });
+    }
+}
+ 
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+ 
+billy.matchInterests(freeMusic);
