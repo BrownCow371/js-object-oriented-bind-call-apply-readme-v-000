c105a6fd73fdcc570dd3ee145021511d0a8c7396
Jamie Brown <jmbrowncow@gmail.com>
2018-09-20 13:26:35 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 // use this to copy code snippets or us your browser's console
 let sally = { name: 'Sally' };
- 
+
 function greet(customer) {
     console.log(customer, "my name is", this.name);
 }
- 
+
 let newGreet = greet.bind(sally);
+
+newGreet('Bob');
